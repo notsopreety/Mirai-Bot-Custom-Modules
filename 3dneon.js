3fac64b0a0d6e1c407b686532d31212f61b38ba2
Samir Badaila <117369007+notsopreety@users.noreply.github.com>
2023-03-24 14:00:40 +0545

    3D neon text

---
+++ 3dneon.js
@@ -0,0 +1,32 @@
+var Key = "Samir";
+module.exports.config = {
+  name: "3dneon",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: `${Key}`,
+  description: "3D neon text",
+  commandCategory: "edit-image",
+  usages: "text",
+  cooldowns: 2,
+};
+module.exports.run = async function ({ api, event, args, Users }) {
+  let { senderID, threadID, messageID } = event;
+  const request = require('request');
+  const fs = require("fs-extra");
+  const axios = require("axios");
+  let pathImg = __dirname + `/cache/${event.threadID}_${event.senderID}.png`;
+  let text = args.join(" ");
+  if (!text) return api.sendMessage(`Wrong fomat\nUse: ${global.config.PREFIX}${this.config.name} text`, event.threadID, event.messageID);
+  let getWanted = (
+    await axios.get(`https://api.botcahx.biz.id/api/textpro/3d-neon-light?text=${text}&apikey=Admin`, {
+      responseType: "arraybuffer",
+    })
+  ).data;
+  fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
+  return api.sendMessage(
+    { attachment: fs.createReadStream(pathImg) },
+    threadID,
+    () => fs.unlinkSync(pathImg),
+    messageID
+  );
+};
