3f9afc133486198fc96b3650eb49c7565bc22954
Samir Badaila <117369007+notsopreety@users.noreply.github.com>
2023-03-24 13:26:34 +0545

    game interface imao

---
+++ game.js
@@ -0,0 +1,36 @@
+var key = "Samir";
+module.exports.config = {
+  name: "game",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: `${key}`,
+  description: "game image edit",
+  commandCategory: "edit-image",
+  usages: "text",
+  cooldowns: 2,
+};
+module.exports.run = async function ({ api, event, args, Users }) {
+  let { senderID, threadID, messageID } = event;
+  const request = require('request');
+  const fs = require("fs-extra");
+  const axios = require("axios");
+  let pathImg = __dirname + `/cache/${event.threadID}_${event.senderID}.png`;
+  let text = args.join(" ")
+  if (!text) return api.sendMessage(`Wrong fomat\nUse: ${global.config.PREFIX}${this.config.name} text`, event.threadID, event.messageID);
+  const text1 = text.substr(0, text.indexOf(' | ')); 
+  const length = parseInt(text1.length)
+  const text2 = text.split(" | ").pop()
+  const length_2 = parseInt(text2.length)
+  let getWanted = (
+    await axios.get(`https://api.botcahx.biz.id/api/textpro/video-game-classic?text=${text1}&text2=${text2}&apikey=Admin`, {
+      responseType: "arraybuffer",
+    })
+  ).data;
+  fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
+  return api.sendMessage(
+    { attachment: fs.createReadStream(pathImg) },
+    threadID,
+    () => fs.unlinkSync(pathImg),
+    messageID
+  );
+};
